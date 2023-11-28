9a64daf30afa377b91acb0301a7cc14ca1ff44d7
pietras125 <30493164+pietras125@users.noreply.github.com>
2023-11-28 13:49:33 +0100

    dodany streamlit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from openai import OpenAI
+import streamlit as st
+from htmlTemplates import bot_template, user_template, css
 import json
 import time
 
@@ -23,20 +25,39 @@ if __name__ == "__main__":
     client = OpenAI()
     assistant_id = "asst_9JuJxQc2y1LB8rjIVwtUifs6"
     thread = client.beta.threads.create()
+    st.set_page_config(page_title="CZAT DŻIPITI", page_icon="🏠")
 
-    user_input = input("\nPytanie: ")
-    while user_input != "q":
-        message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=user_input,
-        )
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistant_id,
-        )
-        run = wait_on_run(run, thread)
-        messages = client.beta.threads.messages.list(thread_id=thread.id)
-        returned_message = get_message_from_json(messages)
-        print("Odpowiedź: " + returned_message)
-        user_input = input("\nPytanie: ")
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # React to user input
+    if prompt := st.chat_input("Zadaj pytanie"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        with st.spinner("Piszę ..."):
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            message = client.beta.threads.messages.create(
+                thread_id=thread.id,
+                role="user",
+                content=prompt,
+            )
+            run = client.beta.threads.runs.create(
+                thread_id=thread.id,
+                assistant_id=assistant_id,
+            )
+            run = wait_on_run(run, thread)
+            messages = client.beta.threads.messages.list(thread_id=thread.id)
+            returned_message = get_message_from_json(messages)
+            response = returned_message
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response})
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
